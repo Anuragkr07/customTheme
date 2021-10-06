63db55ee9a03671311c465235d3c5cab6f26de21
Anuragkr07 <86841935+Anuragkr07@users.noreply.github.com>
2021-10-06 18:40:37 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <p>This is a custom theme for Wordpress</p>
+</footer>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Custom Theme</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+  </head>
+  <body>
+<header>
+  <h1>Custom Theme Development</h1>
+  <ul>
+    <li> <a href="index.php">Page1</a> </li>
+    <li> <a href="page2.php">Page2</a> </li>
+  </ul>
+</header>
